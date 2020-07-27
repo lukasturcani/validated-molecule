232f9d7efc63429c2b210af39e64c8ce0fa50fa5
Lukas Turcani <lukasturcani93@gmail.com>
2020-07-28 01:15:18 +0200

    Added accessor functions.

--- src/Position.purs
+++ src/Position.purs
@@ -2,6 +2,9 @@ module ValidatedMolecule.Position
     ( Position
     , position
     , toArray
+    , x
+    , y
+    , z
     ) where
 
 data Position = Position Number Number Number
@@ -11,3 +14,12 @@ position = Position
 
 toArray :: Position -> Array Number
 toArray (Position x y z) = [x, y, z]
+
+x :: Position -> Number
+x (Position _x _ _) = _x
+
+y :: Position -> Number
+y (Position _ _y _) = _y
+
+z :: Position -> Number
+z (Position _ _ _z) = _z
